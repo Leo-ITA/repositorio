250478c01f2202c83ad097849bf3cfcf68cbd1e0
Leo-ITA <l20321076@acapulco.tecnm.mx>
2024-12-03 11:05:21 -0600

    Add files via upload

---
+++ admin/usuarios/eliminar.php
@@ -0,0 +1,26 @@
+<?php 
+$id = $_GET['id'];
+$usuario = $_GET['usuario'];
+
+// Conexión a la base de datos
+$conexion = mysqli_connect("localhost", "root", "", "repositorio_c116");
+
+// Prepara la consulta para evitar inyecciones SQL
+$consulta = "DELETE FROM usuarios WHERE id='$id'";
+$query = mysqli_query($conexion, $consulta);
+
+$consulta2 = "DELETE FROM documentos WHERE usuario='$usuario'";
+$query = mysqli_query($conexion, $consulta2);
+
+if (isset($_GET['usuario']) && !empty($_GET['usuario'])) {
+    $usuario = $_GET['usuario'];
+    echo "Usuario obtenido correctamente: " . $usuario;
+} else {
+    die("Error: No se ha proporcionado el parámetro 'usuario'.");
+}
+
+
+if($query) {
+    header("location: usuarios.php");
+}
+?>
